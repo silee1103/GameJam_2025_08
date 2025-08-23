bab9d0385b971c8a402c00518b43b82efee4952f
unknown <yangd1009@naver.com>
2025-08-23 17:54:45 +0900

    many things add

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -29,6 +29,10 @@ public class Player : MonoBehaviour, IListener
     {
         Vector2 input = value.Get<Vector2>();
         if (input == Vector2.zero) return;
+        if (MapManager.Instance.TryGetMapInPos((Vector2)transform.position + input, out var info)) //만약 앞에 물이면 못감
+        {
+            
+        }
         EventManager.Instance.PostNotification(EVENT_TYPE.EUserMove, this, input);
         MovingAnimation(input);
     }
--- Assets/Scripts/WaterFlow.cs
+++ Assets/Scripts/WaterFlow.cs
@@ -1,12 +1,24 @@
+using System;
 using UnityEngine;
 
-public class WaterFlow : MonoBehaviour
+public class WaterFlow : Water
 {
+    private void Awake()
+    {
+        pos = transform.position;
+        isFloating = true;
+    }
+
+    public Vector2 FloatingDir
+    {
+        get { return floatingDir; }
+    }
     [SerializeField]
     private Vector2 floatingDir = Vector2.up;
 
     void Start()
     {
-        transform.Rotate(floatingDir - Vector2.up);
+        transform.Rotate(Vector3.forward, (floatingDir.x + floatingDir.y) * Vector2.Angle(Vector2.up, floatingDir));
+        MapManager.Instance.AddMapInfo(this.gameObject, MAP_TYPE.WATER, this);
     }
 }
